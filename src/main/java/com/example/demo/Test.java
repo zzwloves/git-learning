a077fd80068962d0bf074b29999a1679d4c1c757
zhuzw <zhuzw@kltong.com>
2019-09-06 11:23:52 +0800

    咔咔咔咔咔咔

---
+++ src/main/java/com/example/demo/Test.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+/**
+ * @author zhuzw
+ * @version <b>1.0.0</b>
+ * @date 2019/9/6 10:27
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("ssssss");
+    }
+}
