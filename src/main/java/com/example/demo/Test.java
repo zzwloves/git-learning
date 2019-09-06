db14c28bf0eb4f9e97d2c2c5ab0753e4fc9be7a0
zhuzw <zhuzw@kltong.com>
2019-09-06 14:31:39 +0800

    lllll

--- src/main/java/com/example/demo/Test.java
+++ src/main/java/com/example/demo/Test.java
@@ -9,5 +9,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("aaa");
         System.err.println("bbbbbbb");
+        System.err.println("uuuuu");
     }
 }
